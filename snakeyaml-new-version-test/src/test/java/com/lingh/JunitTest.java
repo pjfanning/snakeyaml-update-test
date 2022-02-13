75fafa952408107a91de9887a936780158de5a5c
linghengqian <linghengqian@outlook.com>
2022-02-13 21:13:16 +0800

    add snakeYamlTest4

--- snakeyaml-new-version-test/src/test/java/com/lingh/JunitTest.java
+++ snakeyaml-new-version-test/src/test/java/com/lingh/JunitTest.java
@@ -65,4 +65,23 @@ public class JunitTest {
             }
         }
     }
+
+    @Test
+    public void snakeYamlTest4() {
+        YamlExecuteProcessUnit yamlExecuteProcessUnit = new YamlExecuteProcessUnit();
+        yamlExecuteProcessUnit.setUnitID("159917166");
+        yamlExecuteProcessUnit.setStatus(ExecuteProcessConstants.EXECUTE_STATUS_DONE);
+        Collection<YamlExecuteProcessUnit> unitStatuses = Collections.singleton(yamlExecuteProcessUnit);
+        YamlExecuteProcessContext yamlExecuteProcessContext = new YamlExecuteProcessContext();
+        yamlExecuteProcessContext.setUnitStatuses(unitStatuses);
+        String marshal = new Yaml().dumpAsMap(yamlExecuteProcessContext);
+        YamlExecuteProcessContext unmarshal = new Yaml(new Constructor(YamlExecuteProcessContext.class)).loadAs(marshal,YamlExecuteProcessContext.class);
+        System.out.println("test problem out class ---------" + unmarshal.getUnitStatuses().getClass().getName());
+        for (YamlExecuteProcessUnit unit : unmarshal.getUnitStatuses()) {
+            System.out.println("test problem in class ---------" + unit.getClass().getName());
+            if (unit.getStatus() != ExecuteProcessConstants.EXECUTE_STATUS_DONE) {
+                return;
+            }
+        }
+    }
 }
