c2193674d41b070e60f91aa3d6e8b5c4f07851e4
linghengqian <linghengqian@outlook.com>
2022-02-13 18:41:46 +0800

    add snakeYamlTest3

--- snakeyaml-old-version-test/src/test/java/com/lingh/JunitTest.java
+++ snakeyaml-old-version-test/src/test/java/com/lingh/JunitTest.java
@@ -5,6 +5,8 @@ import com.lingh.pojo.YamlExecuteProcessContext;
 import com.lingh.pojo.YamlExecuteProcessUnit;
 import com.lingh.ssEnum.ExecuteProcessConstants;
 import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -39,6 +41,22 @@ public class JunitTest {
                 "    unitID: '159917166'\n" +
                 "  : null\n";
         YamlExecuteProcessContext unmarshal = YamlEngine.unmarshal(marshal, YamlExecuteProcessContext.class);
+        System.out.println("test problem out class ---------" + unmarshal.getUnitStatuses().getClass().getName());
+        for (YamlExecuteProcessUnit unit : unmarshal.getUnitStatuses()) {
+            System.out.println("test problem in class ---------" + unit.getClass().getName());
+            if (unit.getStatus() != ExecuteProcessConstants.EXECUTE_STATUS_DONE) {
+                return;
+            }
+        }
+    }
+
+    @Test
+    public void snakeYamlTest3() {
+        String marshal = "unitStatuses: !!set\n" +
+                "  ? status: EXECUTE_STATUS_DONE\n" +
+                "    unitID: '159917166'\n" +
+                "  : null\n";
+        YamlExecuteProcessContext unmarshal = new Yaml(new Constructor(YamlExecuteProcessContext.class)).loadAs(marshal,YamlExecuteProcessContext.class);
         System.out.println("test problem out class ---------" + unmarshal.getUnitStatuses().getClass().getName());
         for (YamlExecuteProcessUnit unit : unmarshal.getUnitStatuses()) {
             System.out.println("test problem in class ---------" + unit.getClass().getName());
